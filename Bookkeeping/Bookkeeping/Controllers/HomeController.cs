68ebcb27e172c25ebaf243f26c60ddb261aba98b
YiHsuanLi <th840113@gmail.com>
2017-08-10 13:35:31 +0800

    new project

---
+++ Bookkeeping/Bookkeeping/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookkeeping.Models;
+using PagedList;
+
+namespace Bookkeeping.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RecordDataEntities db = new RecordDataEntities();
+        public ActionResult Index(int? page)
+        {
+            ViewBag.RecordId = 1;
+
+            var searchString = db.RecordDatas.ToList();
+            searchString = db.RecordDatas.ToList();
+
+            return View(searchString.ToPagedList(pageNumber: page ?? 1, pageSize: 10));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
